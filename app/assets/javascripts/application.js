d5a1e68f791d4f5767263bffd52c32cc2bb4d5ab
Rick G <lacunainc04@gmail.com>
2015-11-04 02:56:24 -0800

    Put require jquery in first position on application.js and precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require cocoon
-//= require reveal
 //= require jquery
 //= require jquery_ujs
+//= require cocoon
+//= require reveal
 //= require_tree .
