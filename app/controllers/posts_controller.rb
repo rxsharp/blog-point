db6bb5ba47042c67248af99beeda10afd4bb0077
Rick G <lacunainc04@gmail.com>
2015-12-29 00:35:52 -0800

    Allow logged out users to view posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:show]
   before_filter :require_permission, only: :edit
   respond_to :html
 
